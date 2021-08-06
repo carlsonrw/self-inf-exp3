ef895521a35d6d9b3a2c369eb56e68bc6300394b
Ryan Carlson <31257112+carlsonrw@users.noreply.github.com>
2021-08-06 13:58:02 -0400

    Update and rename database_config.php to write_data.php

--- database_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $servername = "mysqlpipeline.crockettwiki.dreamhosters.com";
-  $port = 3306; ?
-  $username = "pipelineteam";
-  $password = "crockettlab14";
-  $dbname = "crockettlab_pipeline";
-  $table = "test";
-?>
