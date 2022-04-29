4b4387ddac0b50a3adcaa8f5045f6aa90320113c
Ryan Carlson <31257112+carlsonrw@users.noreply.github.com>
2022-04-28 23:30:06 -0400

    Revert "Update and rename database_config.php to write_data.php"
    This reverts commit ef895521a35d6d9b3a2c369eb56e68bc6300394b.

---
+++ database_config.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "mysqlpipeline.crockettwiki.dreamhosters.com";
+  $port = 3306; ?
+  $username = "pipelineteam";
+  $password = "crockettlab14";
+  $dbname = "crockettlab_pipeline";
+  $table = "test";
+?>
